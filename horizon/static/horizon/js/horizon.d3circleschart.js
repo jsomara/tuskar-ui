jsomara/tuskar-ui

commit c263c4339ef66dee004ba489af534fc727f734aa

Adding circles chart
- adding reusable horizon.d3_circles_chart
- adding testing charts to Rack detail

Change-Id: Ia79796fd95e8a25a2d5c678a1746d8270003567e

horizon/static/horizon/js/horizon.d3circleschart.js
@@ -1,0 +1,258 @@
+/*
+    Draw circles chart in d3.
+
+    To use, a div is required with the data attributes
+    data-chart-type="circles_chart", data-url and data-size in the
+    div.
+
+    data-chart-type - must be "circles_chart" so chart gets initialized
+    data-url        - (string) url for the json data for the chart
+    data-time       - (string) time parameter, gets appended to url as time=...
+    data-size       - (integer) size of the circles in pixels
+
+    If used in popup, initialization must be made manually e.g.:
+      addHorizonLoadEvent(function() {
+          horizon.d3_circles_chart.init('.html_element');
+          horizon.d3_circles_chart.init('.health_chart');
+      });
+
+
+    Example:
+      <div class="html_element"
+           data-chart-type="circles_chart"
+           data-url="/infrastructure/racks/1/top_communicating.json?cond=to"
+           data-time="now"
+           data-size="22">
+      </div>
+
+    There are controll elements for the cirles chart, implementing some commands
+    that will be executed over the chart.
+
+    1. The selectbox for time change, implements ChangeTime command. It has to
+    have data attribute data-circles-chart-command="change_time", with defined
+    receiver as jquery selector (selector can point to more elements and it will
+    execute the command on all of them) e.g. data-receiver="#rack_health_chart"
+    Option value is then appended to url and chart is refreshed.
+
+    Example
+    <div class="span3 circles_chart_time_picker">
+      <select data-circles-chart-command="change_time"
+              data-receiver="#rack_health_chart">
+        <option value="now">Now</option>
+        <option value="yesterday">Yesterday</option>
+        <option value="last_week">Last Week</option>
+        <option value="last_month">Last Month</option>
+      </select>
+    </div>
+    <div class="clear"></div>
+
+    2. Bootstrap tabs for switching different circle_charts implement command
+    ChangeUrl. It has to have data attribute data-circles-chart-command="change_url",
+    with defined receiver as jquery selector (selector can point to more elements and
+    it will execute the command on all of them) e.g. data-receiver="#rack_health_chart"
+
+    Inner li a element has to have  attribute data-url, e.g.
+    data-url="/infrastructure/racks/1/top_communicating.json?type=alerts"
+
+    The url of the chart is then switched and chart is refreshed.
+
+    <ul class="nav nav-tabs"
+        data-circles-chart-command="change_url"
+        data-receiver="#rack_health_chart">
+      <li class="active">
+        <a data-url="/infrastructure/racks/1/top_communicating.json?type=overall_health" href="#">
+          Overall Health</a>
+      </li>
+      <li>
+        <a data-url="/infrastructure/racks/1/top_communicating.json?type=alerts" href="#">
+          Alerts</a>
+      </li>
+      <li>
+        <a data-url="/infrastructure/racks/1/top_communicating.json?type=capacities" href="#">
+          Capacities</a>
+      </li>
+      <li>
+        <a data-url="/infrastructure/racks/1/top_communicating.json?type=status" href="#">
+          Status</a>
+      </li>
+    </ul>
+*/
+
+
+horizon.d3_circles_chart = {
+  CirclesChart: function(chart_class, html_element){
+    this.chart_class = chart_class;
+    this.html_element = html_element;
+
+    var jquery_element = $(html_element);
+    this.size = jquery_element.data('size');
+    this.time = jquery_element.data('time');
+    this.url = jquery_element.data('url');
+    this.final_url = this.url;
+    if (this.final_url.indexOf('?') > -1){
+      this.final_url += '&time=' + this.time;
+    }else{
+      this.final_url += '?time=' + this.time;
+    }
+
+    this.time = jquery_element.data('time');
+    this.data = []
+
+    this.refresh = refresh;
+    function refresh(){
+      var self = this;
+      this.jqxhr = $.getJSON( this.final_url, function() {
+            //FIXME add loader in the target element
+          })
+          .done(function(data) {
+            // FIXME find a way how to only update graph with new data
+            // not delete and create
+            $(self.html_element).html("");
+
+            self.data = data
+            self.chart_class.render(self.html_element, self.size, self.data);
+          })
+          .fail(function() {
+            // FIXME add proper fail message
+            console.log( "error" ); })
+          .always(function() {
+            // FIXME add behaviour that should be always done
+          });
+    }
+  },
+  init: function(selector, settings) {
+    var self = this;
+    $(selector).each(function() {
+      self.refresh(this);
+    });
+    self.bind_commands();
+  },
+  refresh: function(html_element){
+    var chart = new this.CirclesChart(this, html_element)
+    // FIXME save chart objects somewhere so I can use them again when
+    // e.g. I am swithing tabs, or if I want to update them
+    // via web sockets
+    // this.charts.add_or_update(chart)
+    chart.refresh();
+  },
+  render: function(html_element, size, data){
+    // FIXME rewrite to scatter plot once we have some cool D3 chart
+    // library
+    var width = size + 4,
+        height = size + 4,
+        round = size / 2;
+        center_x = width / 2,
+        center_y = height / 2;
+
+    var svg = d3.select(html_element).selectAll("svg")
+      .data(data)
+      .enter().append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+    // FIXME use some pretty tooltip from some library we will use
+    // this one is just temporary
+    var tooltip = d3.select(html_element).append("div")
+      .style("position", "absolute")
+      .style("z-index", "10")
+      .style("visibility", "hidden")
+      .style("min-width", "100px")
+      .style("max-width", "110px")
+      .style("min-height", "30px")
+      .style("border", "1px ridge grey")
+      .style("background-color", "white")
+      .text(function(d) { "a simple tooltip"; });
+
+    var circle = svg.append("circle")
+        .attr("r", round)//function(d) { return d.r; })// can be sent form server
+        .attr("cx", center_x)
+        .attr("cy", center_y)
+        .attr("stroke", "black")
+        .attr("stroke-width", function(d) {
+          return 1;
+        })
+        .style("fill", function(d) {return d.color; })
+        .on("mouseover", function(d){tooltip.html("Rack XY <br/>" + d.status);
+                                     tooltip.style("visibility", "visible");})
+        .on("mousemove", function(d){tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+        .on("mouseout", function(d){tooltip.style("visibility", "hidden");});
+        ;
+
+    /*
+    // or just d3 title element
+    circle.append("svg:title")
+      .text(function(d) { return d.x; });
+
+    */
+  },
+  bind_commands: function (){
+    var change_time_command_selector = 'select[data-circles-chart-command="change_time"]';
+    var change_url_command_selector = '[data-circles-chart-command="change_url"]';
+    var self = this;
+    bind_change_time = function(){
+      $(change_time_command_selector).each(function() {
+        $(this).change(function(){
+          var invoker = $(this);
+          var command = new self.Command.ChangeTime(self, invoker);
+          command.execute();
+        });
+      });
+    }
+    bind_change_url = function(){
+      $(change_url_command_selector + ' a').click(function (e) {
+        // Bootstrap tabs functionality
+        e.preventDefault();
+        $(this).tab('show');
+
+        // Command for url change and refresh
+        var invoker = $(this);
+        var command = new self.Command.ChangeUrl(self, invoker);
+        command.execute();
+      })
+    }
+    bind_change_time();
+    bind_change_url();
+  },
+  Command: {
+    ChangeTime: function (chart_class, invoker){
+      // Invoker of the command should know about it's receiver.
+      // Also invoker brings all parameters of the command.
+      this.receiver_selector = invoker.data('receiver');
+      this.new_time = invoker.val();
+
+      this.execute = execute;
+      function execute(){
+          var self = this;
+          $(this.receiver_selector).each(function(){
+            // change time of the chart
+            $(this).data('time', this.new_time);
+            // refresh the chart
+            chart_class.refresh(this)
+          });
+      }
+    },
+    ChangeUrl: function (chart_class, invoker, new_url){
+      // Invoker of the command should know about it's receiver.
+      // Also invoker brings all parameters of the command.
+      this.receiver_selector = invoker.parents('ul').first().data('receiver');
+      this.new_url = invoker.data('url');
+
+      this.execute = execute;
+      function execute(){
+        var self = this;
+        $(this.receiver_selector).each(function(){
+          // change time of the chart
+          $(this).data('url', this.new_url);
+          // refresh the chart
+          chart_class.refresh(this)
+        });
+      }
+    }
+  }
+}
+
+/* init the graphs */
+addHorizonLoadEvent(function() {
+    horizon.d3_circles_chart.init('div[data-chart-type="circles_chart"]');
+});
+

horizon/templates/horizon/_scripts.html
@@ -37,6 +37,7 @@
 <script src='{{ STATIC_URL }}horizon/js/horizon.projects.js' type='text/javascript' charset='utf-8'></script>
 <script src='{{ STATIC_URL }}horizon/js/horizon.networktopology.js' type='text/javascript' charset='utf-8'></script>
 <script src='{{ STATIC_URL }}horizon/js/horizon.d3piechart.js' type='text/javascript' charset='utf-8'></script>
+<script src='{{ STATIC_URL }}horizon/js/horizon.d3circleschart.js' type='text/javascript' charset='utf-8'></script>
 <script src='{{ STATIC_URL }}horizon/js/horizon.capacity.js' type='text/javascript' charset='utf-8'></script>
 <script src='{{ STATIC_URL }}horizon/js/horizon.d3modallinechart.js' type='text/javascript' charset='utf-8'></script>
 {% endcompress %}

openstack_dashboard/dashboards/infrastructure/resource_management/racks/urls.py
@@ -12,7 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls import patterns, url, include
 
 from .views import CreateView, EditView, DetailView, UsageDataView
 
@@ -27,4 +27,5 @@ urlpatterns = patterns(VIEW_MOD,
     url(r'^usage_data$', UsageDataView.as_view(), name='usage_data'),
     url(RACKS % 'edit/', EditView.as_view(), name='edit'),
     url(RACKS % 'detail', DetailView.as_view(), name='detail'),
+    url(RACKS % 'top_communicating.json', 'top_communicating'),
 )

openstack_dashboard/dashboards/infrastructure/resource_management/racks/views.py
@@ -21,6 +21,8 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.core.urlresolvers import reverse
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponse
+
+from django.utils import simplejson
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic import View
 
@@ -109,3 +111,34 @@ class UsageDataView(View):
 
         return HttpResponse(json.dumps(values, cls=DjangoJSONEncoder),
                             mimetype='application/json')
+
+
+def top_communicating(request, rack_id=None):
+    random.seed()
+    data = []
+    statuses = ["OFF", "Good", "Something is wrong", "Disaster"]
+
+    number_of_elements = random.randint(50, 60)
+    for index in range(number_of_elements):
+        status = random.randint(0, 3)
+        percentage = random.randint(0, 100)
+
+        # count black/white color depending to percentage
+        # FIXME encapsulate the algoritm of getting color to the library,
+        # If the algorithm will be used in multiple places. Then pass the
+        # alghoritm name and parameters.
+        # If it will be used only in one place, pass the color directly.
+        color_n = 255 / 100 * percentage
+        color = "rgb(%s, %s, %s)" % (color_n, color_n, color_n)
+
+        data.append({'color': color,
+                     'status': statuses[status],
+                     'percentage': percentage,
+                     'id': "FIXME_RACK id",
+                     'name': "FIXME name",
+                     'url': "FIXME url"})
+
+        data.sort(key=lambda x: x['percentage'])
+
+    return HttpResponse(simplejson.dumps(data),
+        mimetype="application/json")

openstack_dashboard/dashboards/infrastructure/resource_management/templates/resource_management/racks/_detail_overview.html
@@ -81,3 +81,89 @@
     </tr>
   </table>
 </div>
+
+<div class="row-fluid">
+  <div class="span6">
+    <div class="span4">
+      <h4>Top Communicating Racks</h4>
+    </div>
+    <div class="span3 circles_chart_time_picker">
+      <select data-circles-chart-command="change_time"
+              data-receiver="#most_contacting_racks, #most_contacted_racks">
+        <option value="now">Now</option>
+        <option value="yesterday">Yesterday</option>
+        <option value="last_week">Last Week</option>
+        <option value="last_month">Last Month</option>
+      </select>
+    </div>
+    <div class="clear"></div>
+
+    <div class="communication_charts_wrapper">
+      <div class="communication_chart_wrapper">
+        <h5>The most contacting</h5>
+        <div id="most_contacting_racks"
+             class="communication_chart"
+             data-chart-type="circles_chart"
+             data-url="/infrastructure/racks/1/top_communicating.json?cond=from"
+             data-time="now"
+             data-size="22">
+        </div>
+      </div>
+      <div class="communication_chart_connection">-->BG-IMG--></div>
+      <div class="communication_chart_wrapper">
+        <h5>The most contacted</h5>
+        <div id="most_contacted_racks"
+             class="communication_chart"
+             data-chart-type="circles_chart"
+             data-url="/infrastructure/racks/1/top_communicating.json?cond=to"
+             data-time="now"
+             data-size="22">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="span6">
+    <div class="span3">
+      <h4>Host health</h4>
+    </div>
+    <div class="span3 circles_chart_time_picker">
+      <select data-circles-chart-command="change_time"
+              data-receiver="#rack_health_chart">
+        <option value="now">Now</option>
+        <option value="yesterday">Yesterday</option>
+        <option value="last_week">Last Week</option>
+        <option value="last_month">Last Month</option>
+      </select>
+    </div>
+    <div class="clear"></div>
+
+    <ul class="nav nav-tabs"
+        data-circles-chart-command="change_url"
+        data-receiver="#rack_health_chart">
+      <li class="active">
+        <a data-url="/infrastructure/racks/1/top_communicating.json?type=overall_health" href="#">
+          Overall Health</a>
+      </li>
+      <li>
+        <a data-url="/infrastructure/racks/1/top_communicating.json?type=alerts" href="#">
+          Alerts</a>
+      </li>
+      <li>
+        <a data-url="/infrastructure/racks/1/top_communicating.json?type=capacities" href="#">
+          Capacities</a>
+      </li>
+      <li>
+        <a data-url="/infrastructure/racks/1/top_communicating.json?type=status" href="#">
+          Status</a>
+      </li>
+    </ul>
+    <div id ="rack_health_chart"
+         class="health_chart"
+         data-chart-type="circles_chart"
+         data-url="/infrastructure/racks/1/top_communicating.json?cond=health"
+         data-time="now"
+         data-size="40">
+    </div>
+  </div>
+</div>
+

openstack_dashboard/dashboards/infrastructure/static/infrastructure/less/infrastructure.less
@@ -18,6 +18,7 @@ table.capacities{
   }
 }
 
+
 svg {
   .axis {
     path, line {
@@ -26,4 +27,23 @@ svg {
       shape-rendering: crispEdges;
     }
   }
+}
+
+.communication_chart_wrapper {
+  display:inline-block;
+  vertical-align: middle;
+  width: 38%;
+}
+
+.communication_chart_connection {
+  display:inline-block;
+  /*min-width: 100px;
+  max-width: 400px;*/
+  width: 18%;
+  height: 30px;
+  vertical-align: middle;
+}
+
+.circles_chart_time_picker {
+  float: right;
 }

commit 321812d60bde9cbe05a40962ef11a85942c6de9b

Fix the broken qunit test suite
The test suite was broken by using an incorrect init function in horizon.d3circleschart.js introduced in c263c4339ef66dee004ba489af534fc727f734aa

Change-Id: Ib6af2899179752b4c370bdc2012fa53cf74c06e1

horizon/static/horizon/js/horizon.d3circleschart.js
@@ -252,7 +252,7 @@ horizon.d3_circles_chart = {
 }
 
 /* init the graphs */
-addHorizonLoadEvent(function() {
+horizon.addInitFunction(function () {
     horizon.d3_circles_chart.init('div[data-chart-type="circles_chart"]');
 });
 